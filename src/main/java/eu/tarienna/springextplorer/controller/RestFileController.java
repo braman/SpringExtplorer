50f45bc8f545c0264f13f3fa624d3c0d906b81b7
Mark Joachim <mark.joachim@tarienna.eu>
2018-05-02 11:22:30 +0200

    fix some package issues add tarienna banner update version add actuator shutdown

--- src/main/java/eu/tarienna/springextplorer/controller/RestFileController.java
+++ src/main/java/eu/tarienna/springextplorer/controller/RestFileController.java
@@ -1,9 +1,11 @@
 package eu.tarienna.springextplorer.controller;
 
-import java.util.List;
-
-import javax.servlet.http.HttpServletRequest;
-
+import eu.tarienna.springextplorer.conf.Constants;
+import eu.tarienna.springextplorer.dto.FileInfoDTO;
+import eu.tarienna.springextplorer.exception.FileDeleteException;
+import eu.tarienna.springextplorer.exception.FileStoreException;
+import eu.tarienna.springextplorer.exception.FileUnzipException;
+import eu.tarienna.springextplorer.service.StorageService;
 import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,22 +14,12 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.HandlerMapping;
 
-import eu.tarienna.springextplorer.conf.Constants;
-import eu.tarienna.springextplorer.dto.FileInfoDTO;
-import eu.tarienna.springextplorer.exception.FileDeleteException;
-import eu.tarienna.springextplorer.exception.FileStoreException;
-import eu.tarienna.springextplorer.exception.FileUnzipException;
-import eu.tarienna.springextplorer.service.StorageService;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @RestController
 @RequestMapping("api")
@@ -45,8 +37,8 @@ public class RestFileController {
             try {
                 storageService.store(file, uploadDir);
                 //unzip file
-                if (StringUtils.equalsAnyIgnoreCase(file.getContentType(), 
-                        Constants.MIME_APPLICATION_ZIP, 
+                if (StringUtils.equalsAnyIgnoreCase(file.getContentType(),
+                        Constants.MIME_APPLICATION_ZIP,
                         Constants.MIME_APPLICATION_X_ZIP_COMPRESSED)) {
                     storageService.unzip(file, uploadDir);
                 }
